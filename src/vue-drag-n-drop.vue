8b7cfd586f4c9350e3d26ac9f7a6173d3c673d42
Luke <lsh224@lehigh.edu>
2022-01-17 22:49:42 -0500

    saving

--- src/vue-drag-n-drop.vue
+++ src/vue-drag-n-drop.vue
@@ -44,6 +44,7 @@
         class = 'ddDropContainer'
         v-bind:style=" item.season === 'fall' ? 'display: inline-block;' : 'display: inline-block;'">
         <!--- https://stackoverflow.com/questions/48455909/condition-in-v-bindstyle --->
+        <div class = 'ddDropContainer'>
         {{item.name}}
         <Container 
           group-name="col"
@@ -63,6 +64,8 @@
           </Draggable>
         </Container>
         <h3 style="float:right;">Total Credits: {{item.totalCredits}}</h3>
+        </div>
+        <br v-if="item.season === 'fall'">
       </div>
     </div>
 
