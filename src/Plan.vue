44e3f8a4c53e01f59543cfd3d935463e2519e90f
Alibek Kaliyev <alk224@lehigh.edu>
2022-01-18 18:52:22 +0600

    imlemented Google authentication with routing

---
+++ src/Plan.vue
@@ -0,0 +1,189 @@
+<template>
+  <div id="plan">
+    <header>CSB Course Manager</header>
+    <h2>Course management and planning allows to make the most out of your available credits at Lehigh!</h2>
+    <h2>Remember that course plans are fluid and subject to change, you should update your plan as your academic career evolves</h2>
+
+    <button @click="logout">Logout</button>
+    
+    <drag-drop
+      :dropzones="semesters"
+      :dropzonesTitle="'XYZ Company Teams'"
+      :originalData="courses"
+      :originalTitle="'LU Course Plan'"
+      :inPlace="true"
+      :enableSave="true"
+      :enableCancel="true"
+      @dropInOriginalBucket="originalBucketDropEvent"
+      @dropInDestinationBucket="destinationBucketDropEvent"
+      @save="save"
+      @cancel="cancel"
+    >
+      <template #dd-card="{ cardData }">
+        <custom-card
+          :data="cardData"
+          @completed="completedMarked"
+        />
+      </template>
+    </drag-drop>
+  </div>
+</template>
+
+<script>
+/* istanbul ignore file */
+import DragDrop from './vue-drag-n-drop.vue';
+import CustomCard from './components/CustomCard.vue';
+import Auth from './Auth.vue';
+import {getAuth} from 'firebase/auth';
+
+export default {
+  name: 'app',
+  components: {
+    DragDrop,
+    CustomCard,
+    Auth,
+  },
+  data() {
+    return{
+      courses: [
+        {
+          title: 'BUS 001',
+          description: 'Foundations of Business',
+          credits: 1,
+          designation: 'BUS',
+          completed: false,
+          used: false,
+        },
+        {
+          title: 'ECO 001',
+          description: 'Principles of Economics',
+          credits: 4,
+          designation: 'BUS',
+          completed: false,
+          used: false,
+        },
+        {
+          title: 'CSE 007',
+          description: 'Introduction to Programming',
+          credits: 4,
+          designation: 'CS',
+          completed: false,
+          used: false,
+        },
+        {
+          title: 'MATH 021',
+          description: 'Calculus I',
+          credits: 4,
+          designation: 'MATH',
+          completed: false,
+          used: false,
+        },
+        {
+          title: 'PHY 011',
+          description: 'Introduction to Physics I',
+          credits: 4,
+          designation: 'NS',
+          completed: false,
+          used: false,
+        },
+        {
+          title: 'CSE 017',
+          description: 'Programming and Data Structures',
+          credits: 3,
+          designation: 'CS',
+          completed: false,
+          used: false,
+        },
+      ],
+
+      semesters: [
+        {
+          name: 'Semester 1',
+          children: [],
+          totalCredits: 0,
+        },
+        {
+          name: 'Semester 2',
+          children: [],
+          totalCredits: 0,
+        },
+        {
+          name: 'Semester 3',
+          children: [],
+          totalCredits: 0,
+        },
+        {
+          name: 'Semester 4',
+          children: [],
+          totalCredits: 0,
+        }
+      ],
+    }
+  },
+
+  //These are methods for the drag and drop group
+  methods:{
+
+    logout(){
+      getAuth().signOut().then(() => {
+        this.$router.replace('/auth');
+      })
+    },
+
+    getCredits(semesters){
+      semesters.forEach(Element => {
+        console.log(Element);
+        Element.totalCredits = 0;
+        Element.children.forEach(Element1 => {
+          Element.totalCredits = Element1.credits + Element.totalCredits;
+        })
+      })
+    },
+
+    save(received){
+      console.log("Received:", received);
+      console.log(JSON.stringify(this.semesters));
+    },
+
+    completedMarked(data) {
+      if(data.completed == true){
+        data.completed = false;
+      }
+      else{
+        data.completed = true;
+      }
+      alert(data.title);
+    },
+
+    originalBucketDropEvent(result) {
+      console.log("Original: ", result);
+    },
+
+    destinationBucketDropEvent(columnName, result) {
+      console.log("Destination: ", columnName, result)
+      this.getCredits(this.semesters);
+    },
+
+    cancel() {
+      console.log("Cancel hit");
+    }
+  }
+}
+</script>
+
+<style>
+#plan {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+header {
+  font-size: 30px;
+}
+h2 {
+  font-size: 16px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,38 @@
 /* istanbul ignore file */
 import Vue from 'vue'
 import App from './App.vue'
+import Auth from './Auth.vue'
+import router from './router'
+import { initializeApp } from "firebase/app"
+import { getAuth } from 'firebase/auth';
+
+let app = '';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAWrzYelgpTNzmRG2FffV4Cvo94_92ezbg",
+  authDomain: "lehigh-course-plan.firebaseapp.com",
+  projectId: "lehigh-course-plan",
+  storageBucket: "lehigh-course-plan.appspot.com",
+  messagingSenderId: "1006911833931",
+  appId: "1:1006911833931:web:aa196b51684cf65165cace",
+  measurementId: "G-037FB0XFH2"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+getAuth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+});
+
+// new Vue({
+//   router,
+//   render: h => h(Auth)
+// }).$mount('#auth')
